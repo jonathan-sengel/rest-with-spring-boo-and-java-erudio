cb0442e8652e0cd43df478a03ee568f5ee9a8a4e
Jonathan Engel <joow.engeel@gmail.com>
2023-06-01 19:49:08 -0300

    Adding migrations supporte with Flyway

---
+++ 11_MigrationsWithFlyWay/rest-with-spring-boo-and-java-erudio/src/main/resources/db/migration/V2__Populate_Table_Person.sql
@@ -0,0 +1,7 @@
+INSERT INTO `person` (`id`, `address`, `first_name`, `gender`, `last_name`) VALUES
+	(1, 'São Paulo', 'Ayrton', 'Male', 'Senna'),
+	(2, 'Porbandar - India', 'Indira', 'Female', 'Gandhi'),
+	(6, 'São José dos Campos - Brasil', 'Albert', 'Male', 'Richards'),
+	(7, 'Abudabi - Oriente', 'Malek', 'Male', 'Aleki'),
+	(8, 'Mvezo - South Africa', 'Nelson', 'Male', 'Mandela'),
+	(9, 'Smiljan - Croatia', 'Nikola', 'Male', 'Tesla');
